5f5a7bcf0e4babdf9d7f826596d82fd7b3d12589
Charles Rumford <charlesr@isc.upenn.edu>
2016-12-08 17:06:59 -0500

    More setup stuff

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,6 +4,8 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+DEBUG=True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
