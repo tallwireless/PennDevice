fd7fdf1b375bac1678be99fc87421826c0e782a7
Charles Rumford <charlesr@isc.upenn.edu>
2016-12-08 15:38:34 -0500

    Migration to PostGres

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,7 +8,11 @@ ALLOWED_HOSTS += [ 'hussle.net.isc.upenn.edu' ]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'penndevice-dev',
+        'USER': 'penndevice-dev',
+        'PASSWORD': '3c#BmdDCXyp4r!F2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432', 
     }
 }
