1e4c5e0f1d1f4251f239561ac961ef453dbc257d
Charles Rumford <charlesr@isc.upenn.edu>
2017-01-03 13:01:07 -0500

    Removing unneeded code because of AJAX handling

--- registration/views.py
+++ registration/views.py
@@ -84,69 +84,6 @@ def group(request, group_id=None, get_info=None):
     rtn.set_cookie('gid',group_id)
     return rtn
 
-@login_required
-def groupActionAdd(request, group_id=None):
-    get_info = {}
-    error = False
-    for key in request.POST:
-        if key == "csrfmiddlewaretoken":
-            continue
-        key = key.split('-')
-        if key[1] not in get_info:
-            get_info[key[1]] = DeviceFormLine() 
-            get_info[key[1]].index = key[1]
-        if key[0] == 'desc':
-            get_info[key[1]].desc = request.POST["-".join(key)]
-        if key[0] == 'mac':
-            get_info[key[1]].mac = request.POST["-".join(key)]
-        
-    try:
-        grp = DeviceGroup.objects.get(pk=group_id)
-    except Exception:
-        print("FIRE FIRE FIRE")
-        #Something went horribly wrong!
-    else:
-        #iterate over the get_info and deal with it
-        user = request.user
-        for k in get_info:
-            pp(get_info[k])
-            if get_info[k].mac == "":
-                continue
-            if not is_valid_mac(get_info[k].mac):
-                get_info[k].error=error=True
-                get_info[k].err_msg="not vaild mac address"
-                continue
-            else:
-                if len(get_info[k].desc) > 255:
-                    get_info[k].desc = get_info[k].desc[:255]
-            #Let's try to put it into the database
-            try:
-                pf = PacketFence()
-                expires_count = 0
-                if grp.personal:
-                    expires_count = int(Setting.objects.get(pk='personal.default.expire_length').value)
-                else:
-                    expires_count = int(Setting.objects.get(pk='group.default.expire_length').value)
-
-                d = Device.objects.create(
-                        mac_address = get_info[k].mac.lower(),
-                        owner = grp,
-                        added_by = user.username,
-                        added = datetime.utcnow(),
-                        expires = datetime.utcnow()+timedelta(days=expires_count),
-                        description = get_info[k].desc
-                )
-                #Adding it to the PacketFence Server
-                pf.add_node(d,grp)
-                pf.reval_node(d)
-            except Exception as e:
-                print(e)
-                get_info[k].error=error=True
-                get_info[k].err_msg="Issue adding to the database: "+e
-    if error:
-        return group(request, group_id, get_info)
-    return HttpResponseRedirect(reverse('reg:group',args=[group_id]))
-
 
 def ajaxHandler(request): 
     handler = AjaxHandler()
